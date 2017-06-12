083f74d0cd67a693b15201c8cc2cb7b1bb9303ba
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-06-12 12:14:50 -0400

    need to fix choices for admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,11 @@
 # Register your models here. Need to tell admin that Question objects have an admin interface
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+
+
 
 admin.site.register(Question)
+admin.site.register(Choice)
